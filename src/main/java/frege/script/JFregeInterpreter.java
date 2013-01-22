111630d1dcca4ea3ce29e68b7ae279921820705a
mmhelloworld <mmhelloworld@gmail.com>
2013-01-22 18:16:37 -0500

    updated for new Frege runtime and compiler

--- src/main/java/frege/script/JFregeInterpreter.java
+++ src/main/java/frege/script/JFregeInterpreter.java
@@ -1,17 +1,19 @@
 package frege.script;
 
 import java.net.URLClassLoader;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 import frege.prelude.PreludeBase.TEither;
 import frege.prelude.PreludeBase.TList;
 import frege.prelude.PreludeBase.TList.DCons;
 import frege.prelude.PreludeBase.TTuple2;
-import frege.rt.Box;
-import frege.rt.FV;
-import frege.rt.Lambda;
+import frege.runtime.Lazy;
+import frege.runtime.Func1;
+import frege.runtime.Delayed;
 import static frege.script.JavaUtils.*;
 
 /**
@@ -19,48 +21,66 @@ import static frege.script.JavaUtils.*;
  *
  */
 public class JFregeInterpreter {
-	
-	private static final Box.Int WORLD = Box.Int.mk(1);
-	
-	/**
-	 * A wrapper Java interop method for Frege function:
-	 *   frege.script.FregeInterpreter.executeCommand 
-	 * @param script
-	 * @param history
-	 * @return
-	 * @throws Exception
-	 */
-	public static JInterpreterResult interpret(final String term, 
-			final String script, final String moduleName, 
-			final URLClassLoader classLoader) throws Exception {
-		final String newScript;
-		final JInterpreterResult result;
-		final Lambda scriptResultIO = (Lambda) FregeInterpreter.executeCommand(
-				term, Box.mk(script), Box.mk(moduleName), Box.mk(classLoader))._e();
-		final TEither scriptResultEither = (TEither) scriptResultIO.apply(WORLD)._e();
-		if (isLeft(scriptResultEither)) {
-			final String errorMessage = fromFV(getLeft(scriptResultEither));
-			throw new Exception(errorMessage);
-		} else {
-			final TTuple2 res = (TTuple2) getRight(scriptResultEither);
-			result = new JInterpreterResult(res.mem1._e().toString(), res.mem2._e().toString());
-		}
-		return result;
-	}
-	
-	public static URLClassLoader compileScripts(final List<String> scripts,
-			final URLClassLoader loader) throws Exception {
-		final TList scriptsList = fromJavaList(scripts);
-		final Lambda io = (Lambda) FregeInterpreter.compileScripts(
-				scriptsList, Box.mk(loader))._e();
-		final TEither res = (TEither) io.apply(WORLD)._e();
-		if (isLeft(res)) {
-			final TList errors = (TList) getLeft(res);
-			throw new Exception(toJavaList(errors).toString());
-		} else {
-			final URLClassLoader newLoader = fromFV(getRight(res));
-			return newLoader;
-		}
-	}
-	
+  
+  private static final Integer WORLD = 1;;
+  
+  /**
+   * A wrapper Java interop method for Frege function:
+   *   frege.script.FregeInterpreter.executeCommand 
+   * @param script
+   * @param history
+   * @return
+   * @throws Exception
+   */
+  public static JInterpreterResult interpret(final String term, 
+      final String script, final String moduleName, 
+      final URLClassLoader classLoader) throws Exception {
+    final String newScript;
+    final JInterpreterResult result;
+    final Func1 scriptResultIO = (Func1) FregeInterpreter.executeCommand(
+        term, Delayed.delayed(script), Delayed.delayed(moduleName), 
+        Delayed.delayed(classLoader)).forced();
+    final TEither scriptResultEither = (TEither) scriptResultIO.apply(WORLD).forced();
+    if (isLeft(scriptResultEither)) {
+      final String errorMessage = getLeft(scriptResultEither);
+      throw new Exception(errorMessage);
+    } else {
+      final TTuple2 res = (TTuple2) getRight(scriptResultEither);
+      final Object value;
+      if (res.mem1 instanceof Lazy) {
+        value = ((Lazy) res.mem1).forced();
+      } else {
+        value = res.mem1;
+      }
+      result = new JInterpreterResult(
+        value.toString(), 
+        ((Lazy)res.mem2).forced().toString());
+    }
+    return result;
+  }
+  
+  public static URLClassLoader compileScripts(final List<String> scripts,
+      final URLClassLoader loader) throws Exception {
+    final TList scriptsList = fromJavaList(scripts);
+    final Func1 io = (Func1) FregeInterpreter.compileScripts(
+        scriptsList, Delayed.<URLClassLoader>delayed(loader)).forced();
+    final TEither res = (TEither) io.apply(WORLD).forced();
+    if (isLeft(res)) {
+      final TList errors = (TList) getLeft(res);
+      throw new Exception(toJavaList(errors).toString());
+    } else {
+      final URLClassLoader newLoader = getRight(res);
+      return newLoader;
+    }
+  }
+  
+  public static URLClassLoader getClassLoader() {
+          final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+          if (classLoader instanceof URLClassLoader) {
+                  return (URLClassLoader) classLoader;
+          } else {
+                  return new URLClassLoader(new URL[] {}, classLoader);
+          }
+  }
+  
 }
