3e8a41d45f80d6698e9d1a1628afc942857204ce
mmhelloworld <mmhelloworld@gmail.com>
2013-05-21 01:06:22 -0400

    JVM sandbox execution support

--- memory-javac/src/main/java/frege/memoryjavac/JavaUtils.java
+++ memory-javac/src/main/java/frege/memoryjavac/JavaUtils.java
@@ -1,8 +1,18 @@
 package frege.memoryjavac;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.Map;
+import java.util.Scanner;
 
 import javax.script.ScriptException;
 
@@ -14,7 +24,7 @@ import org.eclipse.jdt.internal.compiler.impl.CompilerOptions;
 /**
  * A helper class for Frege module frege.script.FregeInterpreter for Java
  * compilation and reflection on the Frege compiler generated field
- * 
+ *
  */
 public class JavaUtils {
 
@@ -26,9 +36,7 @@ public class JavaUtils {
 
 	/**
 	 * Fetches the value of a variable represented by the "variableName"
-	 * 
-	 * @param javaSource
-	 *            the Java source code
+	 *
 	 * @param className
 	 *            the name of the Java class
 	 * @param variableName
@@ -37,8 +45,9 @@ public class JavaUtils {
 	 *            the class loader
 	 * @return either an error message or the value of the variable
 	 */
-	public static Object fieldValue(final String className, 
-		final String variableName,	final ClassLoader loader) throws ScriptException {
+	public static Object fieldValue(final String className,
+			final String variableName, final ClassLoader loader)
+			throws ScriptException {
 		try {
 			final Class<?> clazz = loader.loadClass(className);
 			return clazz.getDeclaredField(variableName).get(null);
@@ -47,18 +56,56 @@ public class JavaUtils {
 					: new ScriptException(e.getCause().getMessage()));
 		}
 	}
-	
-	public static void invokeMain(final String className, final ClassLoader loader) 
+
+	public static Object spawnFetchFieldValue(final String className,
+			final String variableName, final Map<String, byte[]> classes)
 			throws ScriptException {
+		try {
+			final HttpURLConnection connection =
+					(HttpURLConnection) new URL("http://localhost:9080/tryfrege/run").openConnection();
+			connection.setDoInput(true);
+		    connection.setDoOutput(true);
+		    connection.setUseCaches(false);
+		    connection.setRequestProperty("content-Type", "image/jpeg");
+			connection.setRequestMethod("POST");
+			final OutputStream out = connection.getOutputStream();
+	        final ObjectOutputStream oos = new ObjectOutputStream(
+					new BufferedOutputStream(out));
+			oos.writeObject(className);
+			oos.writeObject(variableName);
+			oos.writeObject(classes);
+			oos.flush();
+			oos.close();
+			try (final Scanner scanner = new Scanner(
+					new BufferedInputStream(connection.getInputStream()), "utf-8")) {
+				scanner.useDelimiter("\\Z");
+				if (scanner.hasNext()) {
+					return scanner.next();
+				} else {
+					throw new ScriptException(
+						"Access Denied or Request timed out! Restarting REPL... Please wait for a few seconds!");
+				}
+			}
+		} catch (final IOException e) {
+			final ScriptException se = new ScriptException(e.getMessage());
+			se.initCause(e);
+			throw se;
+		}
+	}
+
+	public static void invokeMain(final String className,
+			final ClassLoader loader) throws ScriptException {
 		Class<?> clazz;
 		try {
 			clazz = loader.loadClass(className);
-			final Method main = clazz.getDeclaredMethod("main", new Class[] {String[].class});
-			main.invoke(null, new Object[] {new String[] {}});
-		} catch (ClassNotFoundException | IllegalAccessException | 
-				IllegalArgumentException | InvocationTargetException | 
-				NoSuchMethodException | SecurityException e) {
-			final ScriptException exception = new ScriptException(e.getMessage());
+			final Method main = clazz.getDeclaredMethod("main",
+					new Class[] { String[].class });
+			main.invoke(null, new Object[] { new String[] {} });
+		} catch (ClassNotFoundException | IllegalAccessException
+				| IllegalArgumentException | InvocationTargetException
+				| NoSuchMethodException | SecurityException e) {
+			final ScriptException exception = new ScriptException(
+					e.getMessage());
 			exception.initCause(e);
 			throw exception;
 		}
