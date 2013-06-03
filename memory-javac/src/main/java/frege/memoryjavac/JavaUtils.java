fec82382e3a4b2e6b1cc7f671fd2457726fc8d14
mmhelloworld <mmhelloworld@gmail.com>
2013-06-03 02:25:36 -0400

    sandbox execution in the same JVM

--- memory-javac/src/main/java/frege/memoryjavac/JavaUtils.java
+++ memory-javac/src/main/java/frege/memoryjavac/JavaUtils.java
@@ -1,18 +1,22 @@
 package frege.memoryjavac;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FilePermission;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.net.ConnectException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.security.Permission;
 import java.util.Map;
-import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.script.ScriptException;
 
@@ -28,11 +32,7 @@ import org.eclipse.jdt.internal.compiler.impl.CompilerOptions;
  */
 public class JavaUtils {
 
-	private static final String newLine = System.getProperty("line.separator");
-
-	public static URLClassLoader getContextClassLoader() {
-		return (URLClassLoader) Thread.currentThread().getContextClassLoader();
-	}
+	private static final String newLine = System.getProperty("line.separator", "\n");
 
 	/**
 	 * Fetches the value of a variable represented by the "variableName"
@@ -52,45 +52,130 @@ public class JavaUtils {
 			final Class<?> clazz = loader.loadClass(className);
 			return clazz.getDeclaredField(variableName).get(null);
 		} catch (final Throwable e) { // Catch Frege runtime errors
-			throw (e.getCause() == null ? new ScriptException(e.getMessage())
-					: new ScriptException(e.getCause().getMessage()));
+			throw (e.getCause() == null ? new ScriptException(e.toString())
+					: new ScriptException(e.getCause().toString()));
 		}
 	}
 
-	public static Object spawnFetchFieldValue(final String className,
-			final String variableName, final Map<String, byte[]> classes)
+	public static Object secureFetchFieldValue(final String className,
+			final String variableName, final ClassLoader loader)
 			throws ScriptException {
+		final Timer timer = new Timer("terminator");
+		final ReplSecurityManager sm = new ReplSecurityManager();
+		final SecuredThread runner = new SecuredThread(className, variableName,
+				loader, timer, sm);
+		runner.start();
 		try {
-			final HttpURLConnection connection =
-					(HttpURLConnection) new URL("http://localhost:9080/tryfrege/run").openConnection();
-			connection.setDoInput(true);
-		    connection.setDoOutput(true);
-		    connection.setUseCaches(false);
-		    connection.setRequestProperty("content-Type", "image/jpeg");
-			connection.setRequestMethod("POST");
-			final OutputStream out = connection.getOutputStream();
-	        final ObjectOutputStream oos = new ObjectOutputStream(
-					new BufferedOutputStream(out));
-			oos.writeObject(className);
-			oos.writeObject(variableName);
-			oos.writeObject(classes);
-			oos.flush();
-			oos.close();
-			try (final Scanner scanner = new Scanner(
-					new BufferedInputStream(connection.getInputStream()), "utf-8")) {
-				scanner.useDelimiter("\\Z");
-				if (scanner.hasNext()) {
-					return scanner.next();
-				} else {
-					throw new ScriptException(
-						"Access Denied or Request timed out! Restarting REPL... Please wait for a few seconds!");
-				}
-			}
-		} catch (final IOException e) {
-			final ScriptException se = new ScriptException(e.getMessage());
-			se.initCause(e);
-			throw se;
+			runner.join(1000 * 8);
+		} catch (final InterruptedException e) {
 		}
+		sm.setDisabled(true);
+		if (runner.getResult() != null) {
+			if (runner.getResult() instanceof Throwable) {
+				final Throwable e = (Throwable) runner.getResult();
+				throw new ScriptException(
+						e.getCause() != null ? e.getCause().toString() : e.toString());
+			} else {
+				return runner.getResult();
+			}
+		} else {
+			throw new ScriptException(
+					"Timed out or Access Denied! Restarting REPL...");
+		}
+
+	}
+
+	static class ReplSecurityManager extends SecurityManager {
+		private boolean isDisabled;
+
+		public ReplSecurityManager() {
+			isDisabled = false;
+		}
+
+		@Override
+		public void checkPermission(final Permission perm) {
+			if (isDisabled)
+				return;
+			if (Thread.currentThread().getName().equals("terminator"))
+				return;
+			if (perm instanceof RuntimePermission) {
+				final RuntimePermission runtimePerm = (RuntimePermission) perm;
+				if (runtimePerm.getName().equals("accessDeclaredMembers")) {
+					return;
+				}
+			} else if (perm instanceof FilePermission) {
+				final String fileName = perm.getName();
+				// Allow server and frege jars
+				if ((fileName.endsWith(".jar") || fileName.endsWith(".class"))
+						&& (fileName.contains("jetty") || fileName
+								.contains("frege") ||
+								fileName.contains("tomcat"))) {
+					return;
+				}
+
+			}
+			super.checkPermission(perm);
+		}
+
+		public void setDisabled(final boolean isDisabled) {
+			this.isDisabled = isDisabled;
+		}
+
+	}
+
+	private static class SecuredThread extends Thread {
+		final String className;
+		final String variableName;
+		final ClassLoader loader;
+		final ReplSecurityManager sm;
+		final Timer terminator;
+		Object result = null;
+
+		public SecuredThread(final String className, final String variableName,
+				final ClassLoader loader, final Timer terminator,
+				final ReplSecurityManager sm) {
+			this.className = className;
+			this.variableName = variableName;
+			this.loader = loader;
+			this.sm = sm;
+			this.terminator = terminator;
+		}
+
+		@Override
+		public void run() {
+			final SecurityManager old = System.getSecurityManager();
+			System.setSecurityManager(sm);
+			/*
+			 * schedule JVM exit in 10 seconds in case request doesn't complete
+			 */
+			terminator.schedule(jvmExitTask(sm), 1000 * 10);
+			try {
+				final Class<?> clazz = loader.loadClass(className);
+				final Object value = clazz.getDeclaredField(variableName).get(
+						null);
+				result = value;
+			} catch (final Throwable e) { //catch Frege runtime errors
+				terminator.cancel(); // cancel JVM exit timer
+				result = e;
+			}
+			terminator.cancel(); // cancel JVM exit timer
+			sm.setDisabled(true);
+			System.setSecurityManager(old);
+		}
+
+		public Object getResult() {
+			return result;
+		}
+	}
+
+	private static TimerTask jvmExitTask(final ReplSecurityManager sm) {
+		return new TimerTask() {
+			@Override
+			public void run() {
+				sm.setDisabled(true);
+				System.exit(-1);
+			}
+		};
 	}
 
 	public static void invokeMain(final String className,
