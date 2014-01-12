3d23dca02b3d6ca6a8102bb757c279f2a14e0ef0
mmhelloworld <mmhelloworld@gmail.com>
2014-01-12 04:44:07 -0500

    Refactor

--- memory-javac/src/main/java/frege/memoryjavac/JavaUtils.java
+++ memory-javac/src/main/java/frege/memoryjavac/JavaUtils.java
@@ -1,57 +1,14 @@
 package frege.memoryjavac;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.Map;
 
-import javax.script.ScriptContext;
-import javax.script.ScriptException;
-
-/**
- * A helper class for Frege module frege.script.FregeInterpreter for Java
- * compilation and reflection on the Frege compiler generated field
- *
- */
 public class JavaUtils {
 
-	/**
-	 * Fetches the value of a variable represented by the "variableName"
-	 *
-	 * @param className
-	 *            the name of the Java class
-	 * @param variableName
-	 *            the name of the field whose value will be returned
-	 * @param loader
-	 *            the class loader
-	 * @return either an error message or the value of the variable
-	 */
 	public static Object fieldValue(final String className,
-			final String variableName, final MemoryClassLoader loader) {
-		try {
-			final Class<?> clazz = loader.loadClass(className);
-			return clazz.getDeclaredField(variableName).get(null);
-		} catch (final Throwable e) { // Catch Frege runtime errors
-			return null;
-		}
-	}
-
-	public static void invokeMain(final String className,
-			final ClassLoader loader) throws ScriptException {
-		Class<?> clazz;
-		try {
-			clazz = loader.loadClass(className);
-			final Method main = clazz.getDeclaredMethod("main",
-					new Class[] { String[].class });
-			main.invoke(null, new Object[] { new String[] {} });
-		} catch (ClassNotFoundException | IllegalAccessException
-				| IllegalArgumentException | InvocationTargetException
-				| NoSuchMethodException | SecurityException e) {
-			final ScriptException exception = new ScriptException(
-					e.getMessage());
-			exception.initCause(e);
-			throw exception;
-		}
+			final String variableName, final MemoryClassLoader loader) throws Throwable {
+        final Class<?> clazz = loader.loadClass(className);
+        return clazz.getDeclaredField(variableName).get(null);
 	}
 
 	public static void injectValues(final Map<String, Object> bindings,
