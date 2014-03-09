c55a90290b42ceb4ac240f53f09bb023ce11d5ef
mmhelloworld <mmhelloworld@gmail.com>
2014-03-09 03:01:00 -0400

    Upgrade Frege, support for sandboxed stdout

--- frege-interpreter-java-support/src/main/java/frege/interpreter/javasupport/JavaUtils.java
+++ frege-interpreter-java-support/src/main/java/frege/interpreter/javasupport/JavaUtils.java
@@ -1,6 +1,13 @@
 package frege.interpreter.javasupport;
 
+import frege.runtime.Runtime;
+
+import java.io.BufferedReader;
 import java.io.FilePermission;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.lang.reflect.Field;
 import java.security.Permission;
 import java.util.Map;
@@ -24,13 +31,26 @@ public class JavaUtils {
 
     public static Object sandboxFieldValue(final String className,
                                            final String variableName,
+                                           final String stdinStr,
+                                           final StringWriter outWriter,
+                                           final StringWriter errWriter,
                                            final InterpreterClassLoader loader) {
         return sandbox(new FutureTask<>(new Callable<Object>() {
             @Override
             public Object call() throws Exception {
+                prepareRuntime(stdinStr, outWriter, errWriter);
                 return fieldValue(className, variableName, loader);
             }
         }), 5, TimeUnit.SECONDS);
+    }
+
+    private static void prepareRuntime(final String stdinStr, final StringWriter outWriter, final StringWriter errWriter) {
+        final BufferedReader stdin = new BufferedReader(new StringReader(stdinStr + "\n"));
+        final PrintWriter stdout = new PrintWriter(outWriter);
+        final PrintWriter stderr = new PrintWriter(errWriter);
+        Runtime.stdin.set(stdin);
+        Runtime.stdout.set(stdout);
+        Runtime.stderr.set(stderr);
     }
 
     public static <V> V sandbox(final FutureTask<V> task,
